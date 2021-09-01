a3f71e1c240b3ff77e2d70c392f9ffe906e11f7a
Alec Cohan <alcohan@redhat.com>
2021-09-01 12:14:19 -0400

    update-dependency-endpoint-to-append-all-endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const tmp = require('tmp');
+const _ = require('lodash');
 
 class Config {
     constructor() {
@@ -51,26 +52,26 @@ class Config {
     }
 
     DependencyEndpoints() {
-        var dependencyEndpoints = new(Map);
-        if (Config.data.endpoints){
-            Config.data.endpoints.forEach(function (val){
-                if (!dependencyEndpoints.has(val.app)) {
-                    dependencyEndpoints[val.app] = new(Map);
+        var dependencyEndpoints = {};
+        if (Config.data.endpoints) {
+            _.forEach(Config.data.endpoints, val => {
+                if (!_.has(dependencyEndpoints, val.app)) {
+                    dependencyEndpoints[val.app] = {};
                 }
-                dependencyEndpoints[val.app][val.name] = val;
+                dependencyEndpoints[val.app][val.name] = val
             })
         }
         return dependencyEndpoints;
     }
 
     PrivateDependencyEndpoints() {
-        var privateDependencyEndpoints = new(Map);
+        var privateDependencyEndpoints = {};
         if (Config.data.privateEndpoints){
-            Config.data.privateEndpoints.forEach(function (val){
-                if (!privateDependencyEndpoints.has(val.app)) {
-                    privateDependencyEndpoints[val.app] = new(Map);
+            _.forEach(Config.data.privateEndpoints, val => {
+                if (!_.has(privateDependencyEndpoints, val.app)) {
+                    privateDependencyEndpoints[val.app] = {};
                 }
-                privateDependencyEndpoints[val.app][val.name] = val;
+                privateDependencyEndpoints[val.app][val.name] = val
             })
         }
         return privateDependencyEndpoints;
